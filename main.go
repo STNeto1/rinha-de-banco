8a703b674f5ba1c0dbc147b5ec8a086a48348426
STNeto1 <gfreitasneto18@gmail.com>
2024-03-09 22:52:55 -0300

    feat: working

--- main.go
+++ main.go
@@ -2,21 +2,64 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/bytedance/sonic"
 	"github.com/gofiber/fiber/v2"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// DB models
+type DBClient struct {
+	ID      int
+	Limit   int64
+	Balance int64
+}
+
+type DBTransaction struct {
+	ID          int
+	ClientID    int
+	Amount      int64
+	Type        string
+	Description string
+	CreatedAt   pgtype.Timestamp
+}
+
+// Create transaction models
 type TransactionPayload struct {
 	Value       int32  `json:"valor"`
 	Type        string `json:"tipo"`
 	Description string `json:"descricao"`
+}
+
+type CreateTransactionOutput struct {
+	Balance int64 `json:"saldo"`
+	Limit   int64 `json:"limite"`
+}
+
+// Get extract models
+type ExtractOutputModel struct {
+	Saldo struct {
+		Total int64  `json:"total"`
+		Date  string `json:"data_extrato"`
+		Limit int64  `json:"limite"`
+	} `json:"saldo"`
+
+	LastTransactions []TransactionOutput `json:"ultimas_transacoes"`
+}
+
+type TransactionOutput struct {
+	Amount      int64            `json:"valor"`
+	Type        string           `json:"tipo"`
+	Description string           `json:"descricao"`
+	CreatedAt   pgtype.Timestamp `json:"data"`
 }
 
 func main() {
@@ -46,40 +89,78 @@ func main() {
 
 		// Funciona né K
 		if err != nil || id <= 0 || id > 5 {
-			return c.Status(404).SendString("Cliente não encontrado")
+			return c.Status(http.StatusNotFound).SendString("Cliente não encontrado")
 		}
 
 		var body TransactionPayload
 		if err := c.BodyParser(&body); err != nil {
-			return c.Status(400).SendString("Erro ao processar o corpo requisição")
+			return c.Status(http.StatusUnprocessableEntity).SendString("Erro ao processar o corpo requisição")
 		}
 
 		if body.Value < 1 {
-			return c.Status(422).SendString("Valor inválido")
+			return c.Status(http.StatusUnprocessableEntity).SendString("Valor inválido")
 		}
 
 		if body.Description == "" || len(body.Description) > 10 {
-			return c.Status(422).SendString("Descrição inválida")
+			return c.Status(http.StatusUnprocessableEntity).SendString("Descrição inválida")
 		}
 
 		if body.Type != "c" && body.Type != "d" {
-			return c.Status(422).SendString("Tipo inválido")
+			return c.Status(http.StatusUnprocessableEntity).SendString("Tipo inválido")
 		}
 
-		var response json.RawMessage
-		err = dbpool.QueryRow(context.Background(), "select * from create_transaction($1, $2, $3, $4)", id, body.Value, body.Type, body.Description).Scan(&response)
-
+		tx, err := dbpool.BeginTx(c.Context(), pgx.TxOptions{})
 		if err != nil {
+			log.Println("error creating trx", err)
+			return c.Status(http.StatusInternalServerError).SendString("Erro gerando transação")
+		}
+		defer tx.Rollback(c.Context())
+
+		if _, err := tx.Exec(c.Context(), "INSERT INTO transactions (client_id, amount, type, description) VALUES ($1, $2, $3, $4)", id, body.Value, body.Type, body.Description); err != nil {
 			if strings.Contains(err.Error(), "clients_balance_check") {
-				return c.Status(422).SendString("Valor inválido")
+				return c.Status(422).SendString("Saldo inválido pós transação")
 			}
 
 			log.Println("Erro processando transação", err, body)
-
 			return c.Status(400).SendString("Erro ao processar a resposta da requisição ao banco")
 		}
 
-		return c.Status(200).Send(response)
+		if body.Type == "c" {
+			if _, err := tx.Exec(c.Context(), "UPDATE clients SET balance = balance + $1 WHERE id = $2", body.Value, id); err != nil {
+				if strings.Contains(err.Error(), "clients_balance_check") {
+					return c.Status(422).SendString("Saldo inválido pós transação")
+				}
+
+				log.Println("Erro processando transação", err, body)
+				return c.Status(400).SendString("Erro ao processar a resposta da requisição ao banco")
+			}
+		} else {
+
+			if _, err := tx.Exec(c.Context(), "UPDATE clients SET balance = balance - $1 WHERE id = $2", body.Value, id); err != nil {
+				if strings.Contains(err.Error(), "clients_balance_check") {
+					return c.Status(422).SendString("Saldo inválido pós transação")
+				}
+
+				log.Println("Erro processando transação", err, body)
+				return c.Status(400).SendString("Erro ao processar a resposta da requisição ao banco")
+			}
+		}
+
+		var clientData DBClient
+		if err = tx.QueryRow(c.Context(), "select * from clients where id = $1 limit 1", id).Scan(&clientData.ID, &clientData.Limit, &clientData.Balance); err != nil {
+			log.Println("error fetching client data", err)
+			return c.Status(http.StatusInternalServerError).SendString("Erro ao buscar dados do cliente")
+		}
+
+		if err := tx.Commit(c.Context()); err != nil {
+			log.Println("error commiting", err)
+			return c.Status(http.StatusInternalServerError).SendString("Erro ao buscar dados do cliente")
+		}
+
+		return c.Status(200).JSON(CreateTransactionOutput{
+			Balance: clientData.Balance,
+			Limit:   clientData.Limit,
+		})
 	})
 
 	app.Get("/clientes/:id/extrato", func(c *fiber.Ctx) error {
@@ -88,16 +169,53 @@ func main() {
 		// Funciona né K
 		if err != nil || id <= 0 || id > 5 {
 			log.Println("Id param", id, err)
-			return c.Status(404).SendString("Cliente não encontrado")
+			return c.Status(http.StatusNotFound).SendString("Cliente não encontrado")
 		}
 
-		var response json.RawMessage
-		if err := dbpool.QueryRow(context.Background(), "select * from get_extract($1)", id).Scan(&response); err != nil {
-			log.Println("Scan err", err)
-			return c.Status(400).SendString("Erro ao processar a resposta da requisição ao banco")
+		tx, err := dbpool.BeginTx(c.Context(), pgx.TxOptions{})
+		if err != nil {
+			log.Println("error creating trx", err)
+			return c.Status(http.StatusInternalServerError).SendString("Erro gerando transação")
+		}
+		defer tx.Rollback(c.Context())
+
+		var clientData DBClient
+		if err = tx.QueryRow(c.Context(), "select * from clients where id = $1", id).Scan(&clientData.ID, &clientData.Limit, &clientData.Balance); err != nil {
+			log.Println("error fetching client data", err)
+			return c.Status(http.StatusInternalServerError).SendString("Erro ao buscar dados do cliente")
 		}
 
-		return c.Status(200).Send(response)
+		rows, err := tx.Query(c.Context(), "select * from transactions where client_id = $1 order by created_at desc", id)
+		if err != nil {
+			log.Println("error fetching  transactions", err)
+			return c.Status(http.StatusInternalServerError).SendString("Erro ao buscar dados da transação")
+		}
+
+		transactions, err := pgx.CollectRows(rows, pgx.RowToStructByName[DBTransaction])
+		if err != nil {
+			log.Println("error collecting transactions", err)
+			return c.Status(http.StatusInternalServerError).SendString("Erro ao buscar dados da transação")
+		}
+
+		var result ExtractOutputModel
+		result.Saldo.Total = clientData.Balance
+		result.Saldo.Limit = clientData.Limit
+		result.Saldo.Date = time.Now().Format("2006-01-02")
+
+		for _, transaction := range transactions {
+			result.LastTransactions = append(result.LastTransactions, TransactionOutput{
+				Amount:      transaction.Amount,
+				Type:        transaction.Type,
+				Description: transaction.Description,
+				CreatedAt:   transaction.CreatedAt,
+			})
+		}
+
+		if result.LastTransactions == nil {
+			result.LastTransactions = []TransactionOutput{}
+		}
+
+		return c.Status(200).JSON(result)
 	})
 
 	app.Get("/reset", func(c *fiber.Ctx) error {
